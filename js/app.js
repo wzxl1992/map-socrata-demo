a75070f70dd899226a1671c3673119b68cdf794c
Dave Stearns <dlsinfo@uw.edu>
2014-11-18 18:38:57 -0800

    dataset URL and gitignore file

--- js/app.js
+++ js/app.js
@@ -1,6 +1,13 @@
 /*
     app.js
     our application code
+
+    Alternative fuel locations in Chicago dataset:
+    https://data.cityofchicago.org/resource/alternative-fuel-locations.json
+
+    Chicago coordinates:
+    lat: 41.8369
+    lng: -87.6847
  */
 
 "use strict";
